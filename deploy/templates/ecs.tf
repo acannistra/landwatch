cf8fae2d9682f52738832954d206362478726856
Tony Cannistra <tony.cannistra@gmail.com>
2021-06-15 20:35:04 -0700

    IAM role for execution

--- deploy/templates/ecs.tf
+++ deploy/templates/ecs.tf
@@ -7,3 +7,24 @@ resource "aws_ecs_cluster" "app" {
   capacity_providers = ["FARGATE", "FARGATE_SPOT"]
   tags = var.tags
 }
+
+resource "aws_iam_role" "taskexecrole" {
+  name = "ECSTaskExecutionRole"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy" 
+  ]
+}
